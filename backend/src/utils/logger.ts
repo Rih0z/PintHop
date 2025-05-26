122a4a36abe1581cc597063ef068711efcaa11e0
Koki Riho <riho.dare@gmail.com>
2025-05-26 12:32:44 -0700

    feat: add logger utility and tests

---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,27 @@
+/**
+ * プロジェクト: PintHop
+ * ファイルパス: backend/src/utils/logger.ts
+ *
+ * 作成者: Koki Riho and Codex
+ * 作成日: 2025-05-31 00:00:00
+ *
+ * 更新履歴:
+ * - 2025-05-31 00:00:00 Koki Riho and Codex 新規作成
+ *
+ * 説明:
+ * コンソールラッパーによる単純なロギングユーティリティ
+ */
+
+export const logger = {
+  info: (message: string, ...optionalParams: unknown[]): void => {
+    console.log(message, ...optionalParams);
+  },
+  error: (message: string, ...optionalParams: unknown[]): void => {
+    console.error(message, ...optionalParams);
+  },
+  warn: (message: string, ...optionalParams: unknown[]): void => {
+    console.warn(message, ...optionalParams);
+  }
+};
+
+export default logger;
